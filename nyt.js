69591e8e07f6b71a291b2b070822ad6d7f96c081
Your Name <you@example.com>
2020-10-29 19:08:44 -0400

    [nyt.js] nyt portion complete

--- nyt.js
+++ nyt.js
@@ -1,3 +1,16 @@
-console.log(credentials);
+// console.log(credentials);
 
 // Write code here
+
+fetch(`https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=${credentials}`)
+.then(res => res.json())
+.then(json => console.log(json.results))
+
+async function getData(){
+    let results = await fetch(`https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=${credentials}`)
+    json = await results.json()
+    // .then(json => console.log(json.results))
+    console.log(json.results)
+    }
+    
+    getData()
