c40235cf1a0bbf9bc5b1210a2bf20011bc6a5e98
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-10-29 21:12:39 -0500

    [HW] add deliverable

--- nyt.js
+++ nyt.js
@@ -1,3 +1,14 @@
-console.log(credentials);
+console.log(API_KEY);
 
 // Write code here
+
+fetch('https://api.nytimes.com/svc/topstories/v2/science.json?api-key=WOJfAklv0gM51glSKvyZBo6iqAFXIkCI')
+.then(response => {
+    return response.json();
+})
+.then(nytData => {
+    console.log(nytData);
+})
+.catch(error => {
+    console.log(error);
+});
--- other.js
+++ other.js
@@ -1,1 +1,42 @@
 // Write code here
+
+class MyCats {
+    constructor(name, breed, color, personality) {
+        this.name = name;
+        this.breed = breed;
+        this.color = color;
+        this.personality = personality;
+    }
+}
+
+const stella = new MyCats('stella', 'tuxedo', 'black-white', 'mean');
+const piper = new MyCats('piper', 'birman', 'cream', 'sweet');
+
+
+
+class Movies {
+    constructor(title, genre, rating) {
+        this.title = title;
+        this.genre = genre;
+        this.rating = rating;
+    }
+}
+
+const texasChainsawMassacre = new Movies('texasChainsawMassacre', 'horror', 'R');
+const aceVentura = new Movies('aceVentura', 'comedy', 'PG-13');
+
+
+
+
+
+
+// I HAVE NO IDEA WHAT IM SUPPOSED TO DO FOR THE SECOND PART OF THIS PROBLEM AND TA HOURS ARE ALREADY OVER IM SORRY!!!!
+const idk = new Promise(function(resolve, reject) {
+    if (name === stella) {
+        resolve();
+    } else {
+        reject();
+    }
+})
+
+console.log(idk);
