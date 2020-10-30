4a466db69572f60eac9da0256eda0616a79d0cb4
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-10-30 09:14:59 -0400

    [js] better fetch call

--- nyt.js
+++ nyt.js
@@ -1,16 +1,15 @@
 // Write code here
-
-
 console.log(API_KEY);
+
 
 async function nytApi() {
     console.log('I should be before the cats data');
     
-    const res = await fetch('https://www.nytimes.com/search.json?query=news')
+    const res = await fetch('https://api.nytimes.com/svc/mostpopular/v2/shared/1/facebook.json?api-key=DCxFehJ4eM4eYT04XkAxRgvy3XyTFsNE')
     const json1 = await res.json()
     console.log('this is the nyt data', json1);
 nytApi();
 
-fetch('https://www.nytimes.com/search.json?query=news')
+fetch('https://api.nytimes.com/svc/mostpopular/v2/shared/1/facebook.json?api-key=DCxFehJ4eM4eYT04XkAxRgvy3XyTFsNE')
 .then((res) => res.json()).then((json) => console.log(json))
 console.log('this is the nyt data');
