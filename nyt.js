97d3754f1b2c646a13bb4641c9565f248442d5de
Nitish Dayal <contact@nitishdayal.me>
2020-10-30 06:36:26 -0700

    feat: main assignment done

--- nyt.js
+++ nyt.js
@@ -1,3 +1,26 @@
 console.log(credentials);
 
-// Write code here
+const QUERY_STRING = `q=election&api-key=${credentials}`
+
+// With fetch
+/* fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?${QUERY_STRING}`)
+  .then(res => res.json())
+  .then(console.log)
+  .catch(console.error) */
+
+
+// With async/await
+const searchForElectionArticles = async () => {
+  try {
+    const request = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?${QUERY_STRING}`)
+    if (!request.ok) throw new Error(request.statusText)
+
+    return await request.json()
+  } catch (e) {
+    throw e
+  }
+}
+
+searchForElectionArticles()
+  .then((data) => console.log(data))
+  .catch((e) => console.error(e))
--- other.js
+++ other.js
@@ -1,1 +1,30 @@
 // Write code here
+const searchForArticle = async (query) => {
+  try {
+    const request = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${credentials}`)
+    if (!request.ok) throw new Error(request.statusText)
+
+    return await request.json()
+  } catch (e) {
+    throw e
+  }
+}
+
+const getArticleHeadlinesAndBylines = (data) => data.response.docs.map(doc => ({ headline: doc.headline.main, byline: doc.byline.original }))
+
+class Article {
+  constructor({ headline, byline }) {
+    this.headline = headline
+    this.byline = byline
+  }
+
+  static createArticles = (articles) => [].concat(articles).map(article => new Article(article))
+}
+
+const myArticleSearch = searchForArticle('obama')
+myArticleSearch.then((res) => {
+  const articleHeadlinesAndAuthors = getArticleHeadlinesAndBylines(res)
+
+  const articleObjects = Article.createArticles(articleHeadlinesAndAuthors)
+  console.log(articleObjects)
+})
