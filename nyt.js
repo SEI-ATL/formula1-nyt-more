64db971833e1b3448c3195f82c491ca76787e73d
jtreeves <jackson.t.reeves@gmail.com>
2020-10-29 23:02:10 -0400

    [nyt] Fetch data from NYT using their API and console the results

--- nyt.js
+++ nyt.js
@@ -1,3 +1,7 @@
+const credentials = API_KEY;
 console.log(credentials);
 
 // Write code here
+fetch(`https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=${credentials}`)
+.then(response => response.json())
+.then(data => console.log(data));
