a32a516f88c72608f267f4e4aeb6c5c9e06d7a32
jtreeves <jackson.t.reeves@gmail.com>
2020-10-29 23:08:59 -0400

    [nyt] Refactor fetch to involve asynch/await as opposed to then

--- nyt.js
+++ nyt.js
@@ -2,6 +2,17 @@ const credentials = API_KEY;
 console.log(credentials);
 
 // Write code here
-fetch(`https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=${credentials}`)
-.then(response => response.json())
-.then(data => console.log(data));
+
+// Initial setup with 'then':
+// fetch(`https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=${credentials}`)
+// .then(response => response.json())
+// .then(data => console.log(data));
+
+// Altered setup with 'await':
+async function nyt() {
+    const result = await fetch(`https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=${credentials}`);
+    const json = await result.json();
+    console.log(json);
+};
+
+nyt();
