f9b3166b9fb4778a91cd835d2f42020c8923d549
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-10-29 22:57:54 -0400

    [added asynce] and recieved fulfilled

--- nyt.js
+++ nyt.js
@@ -2,3 +2,20 @@ console.log(API_KEY);
 
 
 // Write code here
+fetch(`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${API_KEY}`)
+.then(response => response.json())
+.then(data =>{
+    console.log(data);
+})
+
+
+async function nyt(){
+    const res = await fetch(`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${API_KEY}`)
+const json = await res.json()
+
+return nyt
+}
+
+console.log(nyt());
+
+////returned promise "fulfilled"
