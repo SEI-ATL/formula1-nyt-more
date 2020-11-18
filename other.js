53edf9f8508a45a696c9bcf0b19eb25ddcd45ce6
Paul Williams <paul.d.williamsxiv@gmail.com>
2020-11-18 13:09:35 -0500

    [project] created cards, loaded data from api, as well as created classes and promise

--- other.js
+++ other.js
@@ -5,3 +5,21 @@ class Racer{
         this.rank = rank
     }
 }
+
+
+new Promise((resolve,reject)=>{
+    console.log('initial');
+    async function nyt(){
+        let res = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=computers&api-key=${API_KEY}`)
+        let data = await res.json()
+        data.response.docs.forEach(item=>{
+            console.log(item.abstract);
+        })
+        
+
+    }
+    nyt()
+    resolve()
+})
+
+
